8f376fa547db26498420e03102d57035392c9644
mostafa hassan <mostafahassan88@gmail.com>
2017-02-18 22:03:57 +0200

    *Added environment for the game *Created animation and glow for the fruit *Fixed the snake movement and added a texture * added a snake head

---
+++ Assets/Resources/Scripts/SnakeBodyPart.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class SnakeBodyPart : MonoBehaviour {
+
+	public Transform HeadPart, prevBodyPart;
+	public List<turnData> turnPositions = new List<turnData>();
+	public float movementSpeed = 0.01f;
+	public float minDistance = 1.1f;
+
+	// Use this for initialization
+	void Start () {
+		HeadPart = transform.parent.GetChild (0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	 	if (Input.GetKeyUp (KeyCode.RightArrow)) {
+			turnPositions.Add (new turnData(HeadPart.position, KeyCode.RightArrow));
+		}else if (Input.GetKeyUp (KeyCode.LeftArrow)) {
+			turnPositions.Add (new turnData(HeadPart.position, KeyCode.LeftArrow));
+		}
+	}
+
+	public void handleBodyPartMove(){
+		if (prevBodyPart == null) {
+			if (turnPositions.Count > 0 && V3Equal (transform.position, turnPositions [0].getTurnPos ())) {
+				transform.Rotate (Vector3.up, turnPositions [0].getDirecKey () == KeyCode.LeftArrow ? -90 : 90);
+				if (transform.eulerAngles.y == 90 || transform.eulerAngles.y == 270) {
+					transform.position = new Vector3 (transform.position.x, transform.position.y, turnPositions [0].getTurnPos ().z);
+				} else {
+					transform.position = new Vector3 (turnPositions [0].getTurnPos ().x, transform.position.y, transform.position.z);
+				}
+				turnPositions.RemoveAt (0);
+			}
+
+			transform.position += (transform.rotation * Vector3.forward * movementSpeed);
+		} else {
+			float dis = Vector3.Distance (prevBodyPart.position, transform.position);
+
+			Vector3 newPos = prevBodyPart.position;
+			newPos.y = HeadPart.position.y;
+
+			float T = dis / minDistance * movementSpeed;
+
+			transform.position = Vector3.Lerp (transform.position, newPos, T);
+			transform.rotation = Quaternion.Slerp (transform.rotation, prevBodyPart.rotation, T);
+		}
+	}
+
+	public bool V3Equal(Vector3 a, Vector3 b){
+		return Vector3.Distance(a , b) < movementSpeed;
+	}
+
+	// store the data related to when should the body part take a different direction
+	public class turnData {
+		Vector3 turnPos;
+		KeyCode direcKey;
+
+		public turnData(Vector3 tPos, KeyCode pressedKey){
+			turnPos = tPos;
+			direcKey = pressedKey;
+		}
+
+		public Vector3 getTurnPos(){
+			return turnPos;
+		}
+
+		public KeyCode getDirecKey(){
+			return direcKey;
+		}
+	}
+
+	void OnTriggerEnter(Collider col)
+	{
+		if(col.transform.tag.Equals("snakeHead")){
+			transform.parent.GetComponent<SnakeManager> ().doGameOver();	
+		}
+	}
+}
---
+++ Assets/Resources/Scripts/SnakeManager.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class SnakeManager : MonoBehaviour {
+
+	public List<SnakeBodyPart> snakeBodyParts = new List<SnakeBodyPart>();
+	public bool isGameOver = false;
+	public GameObject gameOver;
+	public GameObject bodyPartPrefab;
+	public Text txtScore;
+
+	List<Vector3> spawnPos = new List<Vector3>();
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating ("moveTheSnake", 0, 0.01f);
+
+		for (int x = -18; x < 19; x++) {
+			for (int z = -18; z < 19; z++) {
+				spawnPos.Add (new Vector3 (x, 0.2f, z));
+			}
+		} 
+
+		spawnFruit (spawnPos);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	// called when ever the snake hit the wall to activate the gameOver Screen
+	public void doGameOver(){
+		gameOver.SetActive (true);
+		isGameOver = true;
+	}
+
+	private void spawnFruit(List<Vector3> positions){
+		List<Vector3> copyPos = new List<Vector3> (positions);
+		for (int i = 0; i < transform.childCount; i++) {
+			copyPos.RemoveAll (Pos=> Vector3.Distance(Pos , transform.position) < 1);
+		}
+			
+		int randomChance = UnityEngine.Random.Range (0, 10);
+		GameObject fruitObj = Instantiate(Resources.Load<GameObject>("Prefabs/Apple")); //("Prefabs/"+((randomChance == 9) ? "Apple": "Pear")));
+		fruitObj.GetComponent<FruitHandler> ().sManager = this;
+		fruitObj.transform.position = copyPos [UnityEngine.Random.Range (0, copyPos.Count)];
+
+	}
+
+	public void snakeAteFruit(int fruitScore){
+		txtScore.text = (int.Parse (txtScore.text) + fruitScore).ToString();
+		spawnFruit (spawnPos);
+		addSnakeBodyPart ();
+	}
+
+	public void addSnakeBodyPart(){
+		Transform prevPart = snakeBodyParts [snakeBodyParts.Count - 1].transform;
+
+		SnakeBodyPart bodyPart = Instantiate (bodyPartPrefab, prevPart.position, prevPart.rotation).GetComponent<SnakeBodyPart> ();
+		bodyPart.transform.SetParent (transform);
+		bodyPart.name = "bodyPart" + transform.childCount;
+		bodyPart.prevBodyPart = prevPart;
+		snakeBodyParts.Add (bodyPart);
+	}
+
+	private void moveTheSnake(){
+		if (isGameOver) {
+			CancelInvoke ("moveTheSnake");
+		} else {
+			foreach (SnakeBodyPart bPart in snakeBodyParts) {
+				bPart.handleBodyPartMove ();
+			}
+		}
+	}
+}
